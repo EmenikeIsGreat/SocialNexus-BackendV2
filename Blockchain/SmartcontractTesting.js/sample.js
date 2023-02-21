e198066bb4ddede6b20ac068f0a0c3925ed86cc3
EmenikeIsGreat <emenikeani3@gmail.com>
2023-02-21 09:16:17 -0500

    added testing

---
+++ Blockchain/SmartcontractTesting.js/sample.js
@@ -0,0 +1,4 @@
+module.exports = function doSomething(){
+
+    return 2
+}
---
+++ Blockchain/SmartcontractTesting.js/sample.test.js
@@ -0,0 +1,7 @@
+const sample = require('./sample')
+
+test('just testing  '),()=>{
+    expect(
+        sample()
+    ).toBe(2)
+}
